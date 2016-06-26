03e161b42c5150bf8fb873ba736f03775011a0cc
James Scovern <jamie.scovern@gmail.com>
2016-06-26 17:19:59 -0600

    Fixes #1 changed line height on the boxes to vertically align text

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -51,16 +51,6 @@ window.onload = function(){
 		checkBoxesLeft: function () {
 			return this.boxesSelected === this.boxesTotal;
 		},
-		resetHTMLBoard: function() {
-			var str;
-			for(var i=1; i<=9; i++) {
-				str = "#box"+i;
-				console.log(str);
-				addText("",str);
-				removeClass(str,"green");
-				removeClass(str,"red");
-			}
-		}
 	};
 
 	function GamePlay() {
@@ -108,141 +98,144 @@ window.onload = function(){
 			this.keepPlaying = true;
 		},
 		startGame: function() {
-			document.querySelector("#box1").addEventListener("click",makeMove);
-			document.querySelector("#box2").addEventListener("click",makeMove);
-			document.querySelector("#box3").addEventListener("click",makeMove);
-			document.querySelector("#box4").addEventListener("click",makeMove);
-			document.querySelector("#box5").addEventListener("click",makeMove);
-			document.querySelector("#box6").addEventListener("click",makeMove);
-			document.querySelector("#box7").addEventListener("click",makeMove);
-			document.querySelector("#box8").addEventListener("click",makeMove);
-			document.querySelector("#box9").addEventListener("click",makeMove);
-			document.querySelector("#resetGame").addEventListener("click",resetGame);
+			document.querySelector("#box1").addEventListener("click",this.makeMove);
+			document.querySelector("#box2").addEventListener("click",this.makeMove);
+			document.querySelector("#box3").addEventListener("click",this.makeMove);
+			document.querySelector("#box4").addEventListener("click",this.makeMove);
+			document.querySelector("#box5").addEventListener("click",this.makeMove);
+			document.querySelector("#box6").addEventListener("click",this.makeMove);
+			document.querySelector("#box7").addEventListener("click",this.makeMove);
+			document.querySelector("#box8").addEventListener("click",this.makeMove);
+			document.querySelector("#box9").addEventListener("click",this.makeMove);
+			document.querySelector("#resetGame").addEventListener("click",this.resetGame);
+			document.querySelector("#submitButton1").addEventListener("click",submitButton1);
+			document.querySelector("#submitButton2").addEventListener("click",submitButton2);
+			player1 = new Player();
+			player1.assignPlayerID(thisGame);
+			thisGame.incrementPlayerCount();
+			player2 = new Player();
+			player2.assignPlayerID(thisGame);
+			thisGame.incrementPlayerCount();
+		},
+
+		resetGame: function() {
+			thisGame.resetKeepPlaying();
+			thisBoard = new Board();
+			thisHTMLBoard.resetHTMLBoard();
+			thisHTMLBoard.addResetGameMessage();
+		},
+
+		makeMove: function() {
+			thisGame.assignBoxClicked(this.getAttribute("data-id"));
+			if(thisBoard["box"+thisGame.boxClicked]==="" && thisGame.keepPlaying) {
+				this.textContent += thisGame.currMarker;
+				thisHTMLBoard.addClass("#box"+thisGame.boxClicked,thisGame.currColor);
+				thisBoard.selectBox(thisGame.boxClicked,thisGame.currPlayer);
+				thisHTMLBoard.addMoveMessage(thisGame.boxClicked);
+				if(thisGame.checkWinner(thisBoard) === thisGame.currPlayer) {
+					thisHTMLBoard.addText(thisGame.player1+": "+player1.winsCount,"#player1Score");
+					thisHTMLBoard.addText(thisGame.player2+": "+player2.winsCount,"#player2Score");
+					thisHTMLBoard.addWinnerMessage();
+				} else if (thisBoard.checkBoxesLeft()) {
+					thisHTMLBoard.addGameOverMessage();
+				}
+
+			thisGame.incrementTurn();
+			thisHTMLBoard.addText("Who's Up: "+thisGame.currPlayer,"#currPlayer");}
 		}
+
 	};
+
+	function HTML() {
+		this.HTMLBoard = document.querySelectorAll(".box");
+		this.messageBoard = document.querySelector('#messageBoard');
+	};
+
+	HTML.prototype = {
+		resetHTMLBoard: function() {
+			var str;
+			for(var i=1; i<=this.HTMLBoard.length; i++) {
+				str = "#box"+i;
+				thisHTMLBoard.addText("",str);
+				thisHTMLBoard.removeClass(str,"green");
+				thisHTMLBoard.removeClass(str,"red");
+			}
+		},
+
+		addClass: function(queryStr,classToAdd) {
+			document.querySelector(queryStr).className += " "+classToAdd;
+		},
+
+		removeClass: function(queryStr,className) {
+			var classes = document.querySelector(queryStr).className;
+			classes = classes.replace(className,"");
+			document.querySelector(queryStr).className = classes;
+		},
+
+		addAutoFocus: function(queryStr) {
+			document.querySelector(queryStr).focus();
+		},
+
+		addText: function(textToAdd,queryStr) {
+			document.querySelector(queryStr).textContent = textToAdd;
+		},
+
+		addMoveMessage: function(boxSelected) {
+			var newDiv = document.createElement('div');
+			newDiv.textContent = thisGame.currPlayer+" just selected Box " + boxSelected;
+			this.messageBoard.insertBefore(newDiv,this.messageBoard.firstChild);
+		},
+
+		addWinnerMessage: function() {
+			var newDiv=document.createElement('div');
+			newDiv.textContent = thisGame.currPlayer+" just won! The overall game score is "+thisGame.player1+": "+player1.winsCount+" to "+thisGame.player2+" "+player2.winsCount;
+			this.messageBoard.insertBefore(newDiv,this.messageBoard.firstChild);
+		},
+
+		addResetGameMessage: function() {
+			var newDiv = document.createElement('div');
+			newDiv.textContent = "The game has been reset!  Start again!";
+			this.messageBoard.insertBefore(newDiv,this.messageBoard.firstChild);
+		},
+
+		addGameOverMessage: function() {
+			var newDiv = document.createElement('div');
+			newDiv.textContent = "There is no winner! Reset the game to play again.";
+			this.messageBoard.insertBefore(newDiv,this.messageBoard.firstChild);
+		}
+	}
 
 	var winningPossibilities = [ [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] ];
 
-	addAutoFocus("#player1Input");	
-	document.querySelector("#submitButton1").addEventListener("click",submitButton1);
-	document.querySelector("#submitButton2").addEventListener("click",submitButton2);
-	
+	var player1;
+	var player2;
 	var thisGame = new GamePlay();
 	thisGame.startGame();
-	var player1 = new Player();
-	player1.assignPlayerID(thisGame);
-	thisGame.incrementPlayerCount();
-
-	var player2 = new Player();
-	player2.assignPlayerID(thisGame);
-	thisGame.incrementPlayerCount();
-
 	var thisBoard = new Board();
+	var thisHTMLBoard = new HTML();
+	thisHTMLBoard.addAutoFocus("#player1Input");	
+
 
 	function submitButton1() {
 		event.preventDefault();
-		addClass("#player1Form","hidden");
-		removeClass("#player2Form","hidden");
+		thisHTMLBoard.addClass("#player1Form","hidden");
+		thisHTMLBoard.removeClass("#player2Form","hidden");
 		player1.assignPlayerName("#player1Input");
-		addAutoFocus("#player2Input");	
+		thisHTMLBoard.addAutoFocus("#player2Input");	
 	}
 
 	function submitButton2() {
 		event.preventDefault();
-		addClass("#player2Form","hidden");
-		removeClass("#leftContainer","hidden");
-		removeClass("#gamePlay","hidden");
+		thisHTMLBoard.addClass("#player2Form","hidden");
+		thisHTMLBoard.removeClass("#leftContainer","hidden");
+		thisHTMLBoard.removeClass("#gamePlay","hidden");
 		player2.assignPlayerName("#player2Input");
 		thisGame.player1=player1.name;
 		thisGame.player2=player2.name;
 		thisGame.incrementTurn();
-		addText(player1.name+": "+player1.winsCount,"#player1Score");
-		addText(player2.name+": "+player2.winsCount,"#player2Score");
-		addText("Who's Up: "+thisGame.currPlayer,"#currPlayer");
+		thisHTMLBoard.addText(player1.name+": "+player1.winsCount,"#player1Score");
+		thisHTMLBoard.addText(player2.name+": "+player2.winsCount,"#player2Score");
+		thisHTMLBoard.addText("Who's Up: "+thisGame.currPlayer,"#currPlayer");
 	}
 
-	function makeMove() {
-		thisGame.assignBoxClicked(this.getAttribute("data-id"));
-		if(thisBoard["box"+thisGame.boxClicked]==="" && thisGame.keepPlaying) {
-			this.textContent += thisGame.currMarker;
-			addClass("#box"+thisGame.boxClicked,thisGame.currColor);
-			thisBoard.selectBox(thisGame.boxClicked,thisGame.currPlayer);
-			addMoveMessage(thisGame.boxClicked);
-			if(thisGame.checkWinner(thisBoard) === thisGame.currPlayer) {
-				addText(thisGame.player1+": "+player1.winsCount,"#player1Score");
-				addText(thisGame.player2+": "+player2.winsCount,"#player2Score");
-				addWinnerMessage();
-			} else if (thisBoard.checkBoxesLeft()) {
-				addGameOverMessage();
-			}
-
-		thisGame.incrementTurn();
-		addText("Who's Up: "+thisGame.currPlayer,"#currPlayer");	
-		} //else {
-			//console.log("already clicked");
-		//}
-	}
-
-	function resetGame() {
-		// for (var i=1; i<=9; i++) {
-		// 	boardObject[i] = "";
-		// 	var str = "#box"+i;
-		// 	addText("",str);
-		// 	removeClass(str,"green");
-		// 	removeClass(str,"red");
-		// }
-		// keepPlaying = true;
-		//thisGame = new GamePlay();
-		thisGame.resetKeepPlaying();
-		thisBoard = new Board();
-		thisBoard.resetHTMLBoard();
-		addResetGameMessage();
-	}
-
-
-	function addClass(queryStr,classToAdd) {
-		document.querySelector(queryStr).className += " "+classToAdd;
-	}
-
-	function removeClass(queryStr,className) {
-		var classes = document.querySelector(queryStr).className;
-		classes = classes.replace(className,"");
-		document.querySelector(queryStr).className = classes;
-	}
-
-	function addAutoFocus(queryStr) {
-		document.querySelector(queryStr).focus();
-	}
-
-	function addText(textToAdd,queryStr) {
-		document.querySelector(queryStr).textContent = textToAdd;
-	}
-
-	function addMoveMessage(boxSelected) {
-		var newDiv = document.createElement('div');
-		newDiv.textContent = thisGame.currPlayer+" just selected Box " + boxSelected;
-		// document.querySelector("#messageBoard").appendChild(newDiv);
-		document.querySelector("#messageBoard").insertBefore(newDiv,document.querySelector("#messageBoard").firstChild);
-	}
-
-	function addWinnerMessage() {
-		var newDiv=document.createElement('div');
-		newDiv.textContent = thisGame.currPlayer+" just won! The overall game score is "+thisGame.player1+": "+player1.winsCount+" to "+thisGame.player2+" "+player2.winsCount;
-		// document.querySelector("#messageBoard").appendChild(newDiv);
-		document.querySelector("#messageBoard").insertBefore(newDiv,document.querySelector("#messageBoard").firstChild);
-	}
-
-	function addResetGameMessage() {
-		var newDiv = document.createElement('div');
-		newDiv.textContent = "The game has been reset!  Start again!";
-		// document.querySelector("#messageBoard").appendChild(newDiv);
-		document.querySelector("#messageBoard").insertBefore(newDiv,document.querySelector("#messageBoard").firstChild);
-	}
-
-	function addGameOverMessage() {
-		var newDiv = document.createElement('div');
-		newDiv.textContent = "There is no winner! Reset the game to play again.";
-		// document.querySelector("#messageBoard").appendChild(newDiv);
-		document.querySelector("#messageBoard").insertBefore(newDiv,document.querySelector("#messageBoard").firstChild);
-	}
 }
